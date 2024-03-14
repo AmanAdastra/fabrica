e736f489ce49465cce8f4be40640d14e8fdb94c2
Aman Khan <amanayankhan78@gmail.com>
2024-03-14 12:31:17 +0530

    Hero Slide Responsive

--- src/components/home/HeroSlide.tsx
+++ src/components/home/HeroSlide.tsx
@@ -5,35 +5,33 @@ export const interFont = Inter({ subsets: ["latin"] })
 
 const HeroSlide = () => {
     return (
-        <div className={'flex flex-wrap h-[554px] w-full lg:px-[120px] relative z-10'}>
-            <div className='h-full w-full md:w-1/2   '>
-                <div className=' lg:mt-[37px] h-[480px] text-[36px] md:text-[40px] text-center md:text-start lg:text-[56px] font-bold w-full '>
+        <div className={'flex flex-wrap h-fit w-full xl:px-[120px] relative z-10'}>
+            <div className=' h-fit w-full lg:w-1/2   '>
+                <div className=' lg:mt-[37px] h-fit lg:h-[480px] text-[36px] lg:text-[56px] text-center lg:text-start font-bold w-full '>
                     <p className=' '>
                         Discover the Latest
                     </p>
                     <p className=''>
                         Furniture Trends
                     </p>
-                    <div className='flex justify-center md:justify-start w-full'>
-                        <img src='/Brush.png' className='relative -left-[3.5rem] md:left-0  md:-top-8 -top-6  ' />
+                    <div className='flex justify-center lg:justify-start w-full'>
+                        <img src='/Brush.png' className='relative -left-[3.5rem] lg:left-0  lg:-top-8 -top-6  ' />
                     </div>
                     <div className='text-gray-400 '>
-                        <p className={'md:mt-[20px] mx-auto md:mx-0 text-xl font-medium w-[400px] md:w-[480px] text-justify' + interFont.className}>
+                        <p className={'lg:mt-[20px] mx-auto lg:mx-0 text-xl font-medium w-[400px] lg:w-[480px] justify-center lg:justify-start px-2' + interFont.className}>
                             Shop the Latest Fashion Items and Stay ahead of the style game
                         </p>
                     </div>
                 </div>
 
             </div>
-            <div className='h-full w-full md:w-1/2'>
-                <div className='w-[85%] h-full'>
-                    <div className='flex justify-around h-1/2 gap-2'>
-                        <img src='/chair.jpg' className='rounded-[33px] h-full ' />
-                        <img src='/couch.png' className='rounded-[33px] h-full' />
-                    </div>
-                    <div className='flex justify-around h-1/2 w-full pt-8'>
-                        <img src='/sofa.png' className='rounded-[33px] h-full w-full ' />
-                    </div>
+            <div className='flex flex-col items-center lg:items-end h-fit w-full lg:w-1/2 mt-12 lg:mt-0'>
+                <div className='flex px-2 items-center gap-2 '>
+                    <img src='/couch.png' className='rounded-2xl  object-cover h-[200px] md:h-[280px] w-[180px] md:w-[350px] ' />
+                    <img src='/chair.jpg' className='rounded-2xl  object-cover h-[200px] md:h-[280px] w-[180px] md:w-[350px] ' />
+                </div>
+                <div className='flex px-2 mt-4 '>
+                    <img src='/sofa.png' className='rounded-2xl object-cover  md:h-[280px] h-[240px]  w-[360px] md:w-[700px]' />
                 </div>
             </div>
         </div>
