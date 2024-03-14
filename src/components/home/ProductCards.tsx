993e5626f911779744511661281c917f2e1cb3f3
Aman Khan <amanayankhan78@gmail.com>
2024-03-14 16:57:14 +0530

    Furniture scroll completed with responsive

--- src/components/home/ProductCards.tsx
+++ src/components/home/ProductCards.tsx
@@ -5,7 +5,7 @@ const products = [
     { id: 1, name: 'Stylish Soft Chair', price: 20, image: 'ChairPic1.png' },
     { id: 2, name: 'Modern Soft Chair', price: 20, image: 'ChairPic2.png' },
     { id: 3, name: 'Comfortable Soft Chair', price: 20, image: 'ChairPic3.png' },
-    { id: 4, name: 'New Soft Chair', price: 20, image: 'ChairPic4.png' }
+    { id: 4, name: 'New Soft Chair', price: 20, image: 'ChairPic4.png' },
 ];
 
 interface Products {
@@ -21,16 +21,12 @@ const ProductCard = (product: Products) => {
             <p className='mt-[32px] text-[#78798E] text-[14px]'>{product.name}</p>
             <div className='flex gap-1'>
                 <div className='h-[10px] w-[10px] rounded-full bg-[#E7C4AB]'>
-
                 </div>
                 <div className='h-[10px] w-[10px] rounded-full bg-[#ABD1E7]'>
-
                 </div>
                 <div className='h-[10px] w-[10px] rounded-full bg-[#CCABE7]'>
-
                 </div>
                 <div className='h-[10px] w-[10px] rounded-full bg-[#AFABE7]'>
-
                 </div>
             </div>
             <p className='mt-[8px] text-[12px] font-bold'>${product.price}</p>
@@ -40,13 +36,12 @@ const ProductCard = (product: Products) => {
 
 const ProductCards = () => {
     return (
-        <div className='flex flex-wrap gap-8 mt-[80px] mx-[120px]'>
-            {products.map(product => (
-                <ProductCard key={product.id} name={product.name} price={product.price} image={product.image} />
-            ))}
-            {products.map(product => (
-                <ProductCard key={product.id} name={product.name} price={product.price} image={product.image} />
-            ))}
+        <div className='flex lg:w-full lg:h-full lg:flex-wrap mt-[80px] mx-5 xl:mx-[120px] overflow-scroll '>
+            <div className='flex lg:flex-wrap gap-5 '>
+                {products.map(product => (
+                    <ProductCard key={product.id} name={product.name} price={product.price} image={product.image} />
+                ))}
+            </div>
         </div>
     );
 };
