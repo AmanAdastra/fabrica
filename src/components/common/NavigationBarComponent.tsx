c26c9bf6a1b0c30df2c38a01c85b8135210df228
Aman Khan <amanayankhan78@gmail.com>
2024-03-14 10:39:00 +0530

    Navigation Bar ResponsiveNess

--- src/components/common/NavigationBarComponent.tsx
+++ src/components/common/NavigationBarComponent.tsx
@@ -12,29 +12,29 @@ const NavigationBarComponent = () => {
     const renderNavItem = (text: string, index: number) => (
         <div
             key={index}
-            className={`flex items-center ${index === activeNavItem ? 'border-b-2 border-[#C900C1]' : ''
+            className={`flex justify-between w-[130px]  items-center ${index === activeNavItem ? 'border-b-2 border-[#C900C1]' : ''
                 }`}
             onClick={() => handleNavItemClick(index)}
         >
-            <span className='mx-[8px] py-[11px] font-bold text-[16px]'>{text}</span>
+            <span className='mx-[2px] py-[11px] font-bold text-[16px] break-keep'>{text}</span>
             <ChevronDown size={24} color={index === activeNavItem ? '#C900C1' : undefined} className='cursor-pointer' />
         </div>
     );
 
     return (
         <>
-            <div className='md:flex justify-between items-center mt-12 h-[64px] mb-[96px] px-[120px] w-full hidden'>
-                <img src='/fabricaLogo.png' alt='Website Logo' className='block' />
-                <div className='flex gap-2 h-[40px]'>
-                    <div className='flex gap-4 mr-20'>
-                        {['Shop Now', 'Home Furniture', 'Living Room', 'About Us', 'Contact Us'].map(
+            <div className='md:flex justify-between  items-center mt-12 h-[64px] mb-[96px] xl:px-[120px] w-full hidden'>
+                <img src='/fabricaLogo.png' alt='Website Logo' className='block mr-2' />
+                <div className='flex justify-between gap-2 h-[40px] '>
+                    <div className='flex gap-2 lg:gap-4 lg:mr-20 w-full'>
+                        {['Shop Now', 'Furniture', 'Living Room', 'About Us', 'Contact Us'].map(
                             (text, index) => renderNavItem(text, index)
                         )}
                     </div>
-                    <div className='flex items-center justify-center gap-2 mr-[9rem]'>
-                        <button className='py-2 px-5 text-white rounded-full bg-[#D020C9]'>Log in</button>
-                        <button className='py-2 px-2 text-[#D020C9] text-[16px] rounded-full border-[#C900C1] border-2'>
-                            Get Started
+                    <div className='flex items-center justify-center gap-2 mx-2 '>
+                        <button className=' h-[40px] w-[84px] text-white     text-[14px] rounded-full bg-[#D020C9]'>Log in</button>
+                        <button className=' h-[40px] w-[98px] text-[#D020C9] text-[14px] rounded-full border-[#C900C1] border-2'>
+                            Get started
                         </button>
                     </div>
                 </div>
