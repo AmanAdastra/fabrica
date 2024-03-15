faeb5f1cd344772701217a5b1dc5d1f9785fd51e
Aman Khan <amanayankhan78@gmail.com>
2024-03-15 15:01:51 +0530

    Update FashionSlide.tsx

--- src/components/home/FashionSlide.tsx
+++ src/components/home/FashionSlide.tsx
@@ -9,13 +9,13 @@ const FashionSlide = () => {
                     <h1 className='font-bold text-[32px] inline-block] mt-10'>Enjoy Our Latest Fashion Trends and Style</h1>
                     <img src='StrokeLine.png' className='absolute lg:-top-[0.5rem] lg:-right-[6rem] -right-[30px] top-[2rem]' />
                 </div>
-                <p className='mt-[14px] text-[18px] '>
-                    Ship our curated collection of fashion items and stay ahead of the fashion game
+                <p className='mt-[14px] text-[18px]'>
+                    Ship our curated collection of fashion items and stay ahead of the fashion game.
                 </p>
                 <div className='flex flex-wrap mt-10'>
                     <div className='w-full lg:w-1/2 mt-5 text-center lg:text-start '>
                         <h4 className='font-bold text-[24px] text-center lg:text-start mb-5'>Quality</h4>
-                        <p className='text-gray-400 '>Our fashion items are crafted with the highest materials and attention to details</p>
+                        <p className='text-gray-400 '>Our fashion items are crafted with the highest materials and attention to details.</p>
                     </div>
                     <div className='w-full lg:w-1/2 mt-5 text-center lg:text-start '>
                         <h4 className='font-bold text-[24px] text-center lg:text-start mb-5'>Style</h4>
@@ -23,7 +23,7 @@ const FashionSlide = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-full lg:w-1/2  '>
+            <div className='w-full lg:w-1/2'>
                 <img src='HeroImage1.png' className='rounded-3xl h-[600px] w-auto mx-auto z-10 mt-10 lg:mt-0 ' />
                 <div className='relative'>
                     <div className="h-[570px] w-[650px] rounded-full -z-10 absolute -top-[48rem] lg:-top-[50rem] lg:-right-[100px]  blur-3xl "
